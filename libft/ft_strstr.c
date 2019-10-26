e197d87729c920742690e9b174ef2bbf8205c4da
Crius Telma <ctelma@ox-c4.21-school.ru>
2019-10-26 13:03:13 +0300

    Final version

---
+++ libft/ft_strstr.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctelma <ctelma@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/07 21:32:59 by ctelma            #+#    #+#             */
+/*   Updated: 2019/09/12 17:38:17 by ctelma           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	return (ft_strnstr(haystack, needle, ft_strlen(haystack)));
+}
---
+++ src/create_list.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_list.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctelma <ctelma@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/12 17:10:25 by ctelma            #+#    #+#             */
+/*   Updated: 2019/10/26 12:42:19 by ctelma           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/fillit.h"
+
+static t_tetra	*create_id(int id, char *s)
+{
+	t_tetra *new;
+
+	new = NULL;
+	if (id)
+	{
+		if (!(new = (t_tetra *)malloc(sizeof(t_tetra))))
+			return (NULL);
+		new->tetramin = ft_tetramin(s);
+		new->id = id;
+		new->next = NULL;
+	}
+	return (new);
+}
+
+t_tetra			*create_list(t_tetra **begin, int id, char *s)
+{
+	t_tetra *tmp;
+
+	if (!*begin)
+	{
+		*begin = create_id(id, s);
+		return (*begin);
+	}
+	tmp = *begin;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = create_id(id, s);
+	return (tmp->next);
+}
